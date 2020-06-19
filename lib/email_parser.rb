08a48f9204622b7a8a8850a1790ca59dbc4ceddd
Angela Stephens <angelastephens317@gmail.com>
2020-06-19 23:52:49 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,13 +5,11 @@
 class  EmailAddressParser
   attr_accessor :name, :csv_emails
   
-  def initialize(email_list)
-    @emails=email_list
-  end
+    def initialize(email_list)
+      @emails=email_list
+    end
    
-  def parse 
-    emails.split("/\, |/).uniq
-  end
-end
-  
+    def parse 
+      emails.split("/\, |/).uniq
+   end
 end 
